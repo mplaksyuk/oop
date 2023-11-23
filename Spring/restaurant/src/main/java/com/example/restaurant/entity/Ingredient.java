51f73e5f0753277bffe113a26033f5d30aaac4e4
Michael Plaksyuk <mplaksyuk@fire>
2023-11-23 15:23:05 +0200

    new enum type

--- Spring/restaurant/src/main/java/com/example/restaurant/entity/Ingredient.java
+++ Spring/restaurant/src/main/java/com/example/restaurant/entity/Ingredient.java
@@ -2,12 +2,16 @@ package com.example.restaurant.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import javax.validation.constraints.NotBlank;
+
+import com.example.restaurant.entity.enums.Unit;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,4 +31,11 @@ public class Ingredient {
     @Column(name = "name", updatable = true, nullable = true)
     @NotBlank(message = "Name is required")
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "unit", updatable = false, nullable = false)
+    private Unit unit = Unit.MILLIGRAM;
+
+    
+    private Integer quantity;
 }
